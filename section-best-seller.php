71aac25d5974a2c82629c698be8f3feb0ad2ca30
Sandra <sandra0924@protonmail.com>
2024-11-14 14:46:50 +0100

    made changes to section best sellers php css

--- section-best-seller.php
+++ section-best-seller.php
@@ -2,25 +2,80 @@
     <div class="inner-wrapper">
         <hgroup>
             <h2>Bäst säljande</h2>
-            <div class="arrows">&lt; &gt;</div>
+            <div class="arrows">
+                <img src="/img/chevron-left.svg">
+                <img src="/img/chevron-right.svg">
+            </div>
         </hgroup>
-        <article class="product">
-            <img>
-            <div class="product-colors">
-                <span class="color">•</span>
-                <span class="color">•</span>
-                <span class="color">•</span>
-                <span class="color">•</span>
-                <span class="color">+1</span>
-            </div>
-            <h4 class="product-name">Tält</h4>
-            <p class="product-price">600kr</p>
-            <div class="product-tags">
-                <span class="tag">Värme</span>
-                <span class="tag">+2</span>
-            </div>
-        </article>
-    </div>
+        <div class="container">
+            <article class="product">
+                <img>
+                <section class="inner-wrapper">
+                    <div class="product-colors">
+                        <div class="color"></div>
+                        <div class="color"></div>
+                        <div class="color"></div>
+                        <div class="color"></div>
+                        <div class="color">+1</div>
+                    </div>
+                    <h4 class="product-name">Tält</h4>
+                    <p class="product-price">600kr</p>
+                    <div class="product-tags">
+                        <div class="tag">Värme</div>
+                        <div class="tag">+2</div>
+                    </div>
+                </section>
+            </article>
+            <article class="product">
+                <img>
+                <div class="product-colors">
+                    <span class="color">•</span>
+                    <span class="color">•</span>
+                    <span class="color">•</span>
+                    <span class="color">•</span>
+                    <span class="color">+1</span>
+                </div>
+                <h4 class="product-name">Tält</h4>
+                <p class="product-price">600kr</p>
+                <div class="product-tags">
+                    <span class="tag">Värme</span>
+                    <span class="tag">+2</span>
+                </div>
+            </article>
+            <article class="product">
+                <img>
+                <div class="product-colors">
+                    <span class="color">•</span>
+                    <span class="color">•</span>
+                    <span class="color">•</span>
+                    <span class="color">•</span>
+                    <span class="color">+1</span>
+                </div>
+                <h4 class="product-name">Tält</h4>
+                <p class="product-price">600kr</p>
+                <div class="product-tags">
+                    <span class="tag">Värme</span>
+                    <span class="tag">+2</span>
+                </div>
+            </article>
+            <article class="product">
+                <img>
+                <div class="product-colors">
+                    <span class="color">•</span>
+                    <span class="color">•</span>
+                    <span class="color">•</span>
+                    <span class="color">•</span>
+                    <span class="color">+1</span>
+                </div>
+                <h4 class="product-name">Tält</h4>
+                <p class="product-price">600kr</p>
+                <div class="product-tags">
+                    <span class="tag">Värme</span>
+                    <span class="tag">+2</span>
+                </div>
+            </article>
+        </div><!-- .container -->
+    </div><!-- .inner-wrapper -->
 </section>
 
 
@@ -28,7 +83,10 @@
     <div class="inner-wrapper">
         <hgroup>
             <h2>Nya artiklar</h2>
-            <div class="arrows">&lt; &gt;</div>
+            <div class="arrows">
+                <img src="/img/chevron-left.svg">
+                <img src="/img/chevron-right.svg">
+            </div>
         </hgroup>
         <article class="product">
             <img>
