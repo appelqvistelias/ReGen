4efd3466de6193d9395f88d356606d547f7967ad
Elias Appelqvist <eliasappelqvist@gmail.com>
2024-11-14 14:44:30 +0100

    added styling to header

--- header.php
+++ header.php
@@ -11,6 +11,10 @@
     <link href="styles/footer.css" rel="stylesheet">
     <link href="styles/section-categories.css" rel="stylesheet">
     <link href="styles/section-best-seller.css" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <title>ReGen</title>
 </head>
 
@@ -18,14 +22,23 @@
     <header>
         <div class="inner-wrapper">
             <div class="logo">
-                <img src="img/regen-logo.png" alt="regen logo">
+                <img src="img/regen-logo.svg" alt="regen logo">
             </div>
             <nav>
-                <div id="hamburger-menu"></div>
-                <div id="search-icon"></div>
-                <div id="user-icon"></div>
-                <div id="shopping-cart-icon"></div>
+                <div class="menu">
+                    <div id="hamburger-menu"></div>
+                    <p class="menu-paragraph">Meny</p>
+                </div>
+                <div class="icon-container">
+                    <div id="globe"></div>
+                    <div id="user-icon"></div>
+                    <div id="shopping-cart-icon"></div>
+                </div>
             </nav>
+            <div class="search-field">
+                <input class="search" placeholder="Sök..."> <button class="search-btn"></button>
+            </div>
+
         </div>
 
         <section id="hero" class="hero">
