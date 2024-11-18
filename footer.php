a0327d2af9f7d63b8b1e6ae20057f2aaeebb5c0b
Elias Appelqvist <eliasappelqvist@gmail.com>
2024-11-18 15:48:42 +0100

    styled footer

--- footer.php
+++ footer.php
@@ -1,12 +1,18 @@
 <footer>
     <div class="inner-wrapper">
-        <details>
-            <summary>Vanliga frågor</summary>
-        </details>
-        <details>
-            <summary>Om ReGen</summary>
-        </details>
-        <p>&copy; 2024 ReGen. All rights reserved.</p>
+        <div class="logo">
+            <img src="img/Regen-logo-inverted.svg" alt="regen logo">
+        </div>
+
+        <div class="details-footer">
+            <details>
+                <summary>Vanliga frågor</summary>
+            </details>
+            <details>
+                <summary>Om ReGen</summary>
+            </details>
+        </div>
+        <p class="copyright">&copy; 2024 ReGen. All rights reserved.</p>
     </div>
 </footer>
 
