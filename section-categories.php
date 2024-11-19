4cda07888d6fcdbe89746ebc7c4f75152f79d939
Sandra <sandra0924@protonmail.com>
2024-11-19 16:31:35 +0100

    made changes to stylesheets

--- section-categories.php
+++ section-categories.php
@@ -10,18 +10,23 @@
         <div class="container">
             <div class="product item-card">
                 <div id="tent-card"></div>
+                <p>Tält</p>
             </div>
             <div class="product item-card">
                 <div id="tent-card"></div>
+                <p>Mössor</p>
             </div>
             <div class="product item-card">
                 <div id="tent-card"></div>
+                <p>Sovsäckar</p>
             </div>
             <div class="product item-card">
                 <div id="tent-card"></div>
+                <p>Fiskespö</p>
             </div>
             <div class="product item-card">
                 <div id="tent-card"></div>
+                <p>Skor</p>
             </div>
         </div><!-- .container -->
     </div><!-- .inner-wrapper -->
