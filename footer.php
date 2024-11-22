902c4c6a123b51f310255930e18b74f87dbffed8
Sandra <sandra0924@protonmail.com>
2024-11-22 14:43:52 +0100

    footer for desktop

--- footer.php
+++ footer.php
@@ -1,9 +1,10 @@
 <footer>
+    <div id="back-to-top" onclick="backToTopFunction()">
+        <img src="img/arrow-up-inverted.svg">
+        <p class="tiny">Till toppen</p>
+    </div>
+
     <div class="inner-wrapper">
-        <div id="back-to-top" onclick="backToTopFunction()">
-            <img src="img/arrow-up-inverted.svg">
-            <p class="tiny">Till toppen</p>
-        </div>
 
         <div class="logo">
             <img src="img/Regen-logo-orange.svg" alt="regen logo">
@@ -11,13 +12,26 @@
 
         <div class="details-footer">
             <div class="footer-drop-down">
-                <p>Vanliga Frågor</p><img src="img/chevron-down-inverted.svg">
+                <h3>Vanliga Frågor</h3><img src="img/chevron-down-inverted.svg">
+                <ul>
+                    <li><a href="">Om oss</a></li>
+                    <li><a href="">Hållbarhet</a></li>
+                    <li><a href="">Betalningsalternativ</a></li>
+                    <li><a href="">Frakt och returer</a></li>
+                    <li><a href="">FAQ</a></li>
+                </ul>
             </div>
             <div class="footer-drop-down">
-                <p>Om ReGen</p><img src="img/chevron-down-inverted.svg">
+                <h3>Om ReGen</h3><img src="img/chevron-down-inverted.svg">
+                <p><strong>ReGen AB</strong></p>
+                <p>Storgatan 45, 3tr<br />411 23 Göteborg</p>
+                <p>kontakt@regen.se</p>
+                <p>www.regen.se</p>
             </div>
         </div>
-    </div>
+
+    </div><!-- .inner-wrapper -->
+
     <p class="copyright">&copy; 2024 ReGen. All rights reserved.</p>
 </footer>
 
