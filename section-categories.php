e1457eaa6e65184fe56826a46a4ee0f4090bbb42
Elias Appelqvist <eliasappelqvist@gmail.com>
2024-11-26 17:28:49 +0100

    moved arrows into categories section

--- section-categories.php
+++ section-categories.php
@@ -2,32 +2,32 @@
     <div class="inner-wrapper">
         <hgroup>
             <h2>Kategorier</h2>
-            <div class="arrows">
-                <img src="/img/chevron-left.svg" class="prev-btn" alt="Previous">
-                <img src="/img/chevron-right.svg" class="next-btn" alt="Next">
-            </div>
         </hgroup>
-        <div class="container">
-            <div class="product item-card">
-                <img src="/img/c-tent.jpg" alt="Kategori tält" />
-                <p>Tält</p>
-            </div>
-            <div class="product item-card">
-                <img src="/img/c-mossor.jpg" alt="Kategori mössor" />
-                <p>Mössor</p>
-            </div>
-            <div class="product item-card">
-                <img src="/img/c-sovsackar.jpeg" alt="Kategori sovsäckar" />
-                <p>Sovsäckar</p>
-            </div>
-            <div class="product item-card">
-                <img src="/img/c-fiskespo.jpeg" alt="Kategori fiskespön" />
-                <p>Fiskespön</p>
-            </div>
-            <div class="product item-card">
-                <img src="/img/c-skor.png" alt="Kategori skor" />
-                <p>Skor</p>
-            </div>
-        </div><!-- .container -->
+        <div class="arrows">
+            <img src="/img/chevron-left.svg" class="prev-btn" alt="Previous">
+            <div class="container">
+                <div class="product item-card">
+                    <img src="/img/c-tent.jpg" alt="Kategori tält" />
+                    <p>Tält</p>
+                </div>
+                <div class="product item-card">
+                    <img src="/img/c-mossor.jpg" alt="Kategori mössor" />
+                    <p>Mössor</p>
+                </div>
+                <div class="product item-card">
+                    <img src="/img/c-sovsackar.jpeg" alt="Kategori sovsäckar" />
+                    <p>Sovsäckar</p>
+                </div>
+                <div class="product item-card">
+                    <img src="/img/c-fiskespo.jpeg" alt="Kategori fiskespön" />
+                    <p>Fiskespön</p>
+                </div>
+                <div class="product item-card">
+                    <img src="/img/c-skor.png" alt="Kategori skor" />
+                    <p>Skor</p>
+                </div>
+            </div><!-- .container -->
+            <img src="/img/chevron-right.svg" class="next-btn" alt="Next">
+        </div><!-- .arrows -->
     </div><!-- .inner-wrapper -->
 </section>
